8d1d51a34d4bcfcccb67abf4181bdea54e682850
“Dean” <467271883@qq.com>
2019-07-23 21:50:16 +0800

    Add Welcome component

---
+++ src/page/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="welcome">
+    <div>
+      <h1 style="color: white">TODO LIST</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Welcome"
+  }
+</script>
+
+<style scoped>
+#welcome{
+  background-image: linear-gradient(to top, #fff,#001529);
+  height:100%;
+  width:100%;
+  position:fixed;
+}
+</style>
